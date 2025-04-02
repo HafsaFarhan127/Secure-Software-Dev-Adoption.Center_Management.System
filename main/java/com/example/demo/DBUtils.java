79a876bf8de55b3508858701d15f1b85daaff7c3
Ayoub Feki <60102592@udst.edu.qa>
2025-04-02 08:02:51 +0300

    added most of the screens the ones left are for reset password and some button functonalities like the back button for manager after they register a new user

---
+++ main/java/com/example/demo/DBUtils.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import java.sql.*;
+
+public class DBUtils {
+    private static String url = "jdbc:mysql://localhost:3306/ssw_project";
+    private static String appUsername = "root";  //here i changed from root to user
+    private static String appPassword = ""; //insecure practice but i put a password also and am hardcoding here but its for dev purposes
+
+    public static Connection establishConnection(){
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection(url, appUsername, appPassword);
+            //this is just like web2 in connecting to the dbms
+            System.out.println("Connection Successful");
+        }catch(SQLException e){
+            System.out.println(e.getMessage());
+        }
+        return con;
+    }
+    public static void closeConnection(Connection con,Statement stmt){
+        try{
+            stmt.close(); //statement is this
+            con.close();
+            System.out.println("Connection is closed");
+        }catch(SQLException e){
+            e.getMessage();
+        }
+    }
+}
+
+
---
+++ main/java/com/example/demo/Manager.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+import java.io.IOException;
+
+public class Manager {
+    @FXML
+    private Label welcomeLabel;
+    @FXML
+    private Button logOutButton;
+    private Stage stage;
+
+    public void registerPage(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("addUser.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void logOutAction(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("loginScreen.fxml"));
+        Parent root = loader.load();
+
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void setUsername(String username){
+        welcomeLabel.setText("Welcome, "+username);
+    }
+}
