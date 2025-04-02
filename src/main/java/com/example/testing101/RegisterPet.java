2ac13c887671b40cdbfe7c313812a4218113c0e0
Hafsa Farhan <hafsafarhan127@gmail.com>
2025-04-03 00:32:28 +0300

    testing the push

--- src/main/java/com/example/testing101/RegisterPet.java
+++ src/main/java/com/example/testing101/RegisterPet.java
@@ -1,4 +1,11 @@
 package com.example.testing101;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class RegisterPet {
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField dobField;
 }
